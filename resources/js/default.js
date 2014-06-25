964f142a0e3c8ffc2184fa9910fd25101629faeb
Jason McCallister <themccallister@gmail.com>
2014-06-25 13:01:47 -0700

    Move to default.js

--- resources/js/default.js
+++ resources/js/default.js
@@ -1,6 +1,6 @@
 (function($) {
 
-Craft.SproutSeoTemplate = Garnish.Base.extend(
+Craft.SproutSeoDefault = Garnish.Base.extend(
 {
     // DEFINE VARIABLES
     $selectTwitterDropdown: null,
@@ -18,9 +18,9 @@ Craft.SproutSeoTemplate = Garnish.Base.extend(
             // console.log(this.$selectDropdowns);
 
         // @TODO remove classes from oldDropdowns as THIS method doesn't work
-        this.$oldFacebookDropdown = $('#template-facebook select[name="template_fields[oGType]"]').val();
+        this.$oldFacebookDropdown = $('#default-facebook select[name="default_fields[oGType]"]').val();
         $('#facebook-' + this.$oldFacebookDropdown).removeClass('hidden');
-        this.$oldTwitterDropdown = $('#template-twitter select[name="template_fields[twitterCard]"]').val();
+        this.$oldTwitterDropdown = $('#default-twitter select[name="default_fields[twitterCard]"]').val();
         $('#twitter-' + this.$oldTwitterDropdown).removeClass('hidden');
 
         // @DONE Add listener for this.$selectTwitterDropdown to fire onChange
@@ -32,19 +32,19 @@ Craft.SproutSeoTemplate = Garnish.Base.extend(
     {
 
         // Perform actions only if bacebook is the open tab
-        if ( $('#template-twitter').hasClass('hidden') && !$('#template-facebook').hasClass('hidden') )
+        if ( $('#default-twitter').hasClass('hidden') && !$('#default-facebook').hasClass('hidden') )
         {
             // alert('hello facebook');
-            this.$selectDropdown = $('#template-facebook select[name="template_fields[oGType]"]');
+            this.$selectDropdown = $('#default-facebook select[name="default_fields[oGType]"]');
             this.$appendage = '#facebook-';
             this.$oldSelectOption = this.$selectDropdown.val();
             this.$targetedElement = this.$appendage + this.$oldSelectOption;
             $(this.$targetedElement).removeClass('hidden');
         }
-        else if ( $('#template-facebook').hasClass('hidden') && !$('#template-twitter').hasClass('hidden') )
+        else if ( $('#default-facebook').hasClass('hidden') && !$('#default-twitter').hasClass('hidden') )
         {
             // alert('hello twitter');
-            this.$selectDropdown = $('#template-twitter select[name="template_fields[twitterCard]"]');
+            this.$selectDropdown = $('#default-twitter select[name="default_fields[twitterCard]"]');
             this.$appendage = '#twitter-';
             this.$oldSelectOption = this.$selectDropdown.val();
             this.$targetedElement = this.$appendage + this.$oldSelectOption;
