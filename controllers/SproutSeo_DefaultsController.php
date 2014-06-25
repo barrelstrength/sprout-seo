4114eea4dad9b90a4261b975fb05a4dc6f3e2d2c
Jason McCallister <themccallister@gmail.com>
2014-06-25 12:29:41 -0700

    Add DefaultsController

---
+++ controllers/SproutSeo_DefaultsController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Craft;
+
+class SproutSeo_DefaultsController extends BaseController
+{
+    public function actionNewDefault()
+    {
+        die('new default');
+    }
+
+    public function actionEditDefault()
+    {
+        die('edit default');
+    }
+}
