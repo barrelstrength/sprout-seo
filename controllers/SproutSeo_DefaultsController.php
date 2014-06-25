457ebea71297c4013bc4fba30f16841e8717c4c3
Jason McCallister <themccallister@gmail.com>
2014-06-25 13:25:14 -0700

    Stupid Github App

---
+++ controllers/SproutSeo_DefaultsController.php
@@ -0,0 +1,57 @@
+<?php
+namespace Craft;
+
+class SproutSeo_DefaultsController extends BaseController
+{
+    public function actionNewDefault()
+    {
+        $this->renderTemplate('sproutSeo/defaults/_edit');
+    }
+
+    public function actionEditDefault()
+    {
+        // add the assets stuff here
+        die('edit default');
+    }
+
+    public function actionSaveDefaults()
+    {
+        $this->requirePostRequest();
+
+        $id = false; // we assume have a new item now
+
+        $model = craft()->sproutSeo_meta->newMetaModel($id);
+
+        $defaultFields = craft()->request->getPost('default_fields');
+
+        // Convert Checkbox Array into comma-delimited String
+        if (isset($defaultFields['robots']))
+        {
+            $defaultFields['robots'] = craft()->sproutSeo_meta->prepRobotsForDb($defaultFields['robots']);
+        }
+
+        $model->setAttributes($defaultFields);
+
+        if (craft()->sproutSeo_meta->saveDefaultInfo($model))
+        {
+            craft()->userSession->setNotice(Craft::t('New default saved.'));
+            $this->redirectToPostedUrl();
+        }
+
+        craft()->userSession->setError(Craft::t("Couldn't save the default."));
+
+        // Send the field back to the template
+        craft()->urlManager->setRouteVariables(array(
+            'default' => $model
+        ));
+    }
+
+    public function actionDeleteDefaults()
+    {
+        $this->requirePostRequest();
+        $this->requireAjaxRequest();
+
+        $this->returnJson(array(
+                'success' => craft()->sproutSeo_meta->deleteDefault(craft()->request->getRequiredPost('id')) >= 0 ? true : false));
+    }
+}
