bcffc3e28a1edec353c0f167e0fb1794eb156894
Jason McCallister <themccallister@gmail.com>
2014-07-21 13:49:15 -0700

    Cleanup

--- helpers/BSDPluginHelper.php
+++ helpers/BSDPluginHelper.php
@@ -11,10 +11,10 @@ class BSDPluginHelper
 	{
 		// The plugin name override
 		$plugin = craft()->db->createCommand()
-												 ->select('settings')
-												 ->from('plugins')
-												 ->where('class=:class', array(':class'=> $plugin->getClassHandle(__CLASS__)))
-												 ->queryScalar();
+			->select('settings')
+			->from('plugins')
+			->where('class=:class', array(':class'=> $plugin->getClassHandle(__CLASS__)))
+			->queryScalar();
 
 		$plugin = json_decode( $plugin, true );
 		$pluginNameOverride = $plugin['pluginNameOverride'];
