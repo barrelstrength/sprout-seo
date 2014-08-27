ea6260ebc540bc89bc64e4fcd838808349f9c1b7
Ben Parizek <ben@barrelstrengthdesign.com>
2014-08-27 07:02:16 -0400

    Fix migration name

--- migrations/m140623_000000_sproutSeo_addTwitterCardFieldsToOverrides.php
+++ migrations/m140623_000000_sproutSeo_addTwitterCardFieldsToOverrides.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
  */
-class m140623_000000_sproutSeo_addTwitterCardFieldsToTemplates extends BaseMigration
+class m140623_000000_sproutSeo_addTwitterCardFieldsToOverrides extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
