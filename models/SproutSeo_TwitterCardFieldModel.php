2fc58f056b3af7d54415ee0bef2876e81785f15b
Ben Parizek <ben@barrelstrengthdesign.com>
2014-08-27 07:55:13 -0400

    Cleanup spacing

--- models/SproutSeo_TwitterCardFieldModel.php
+++ models/SproutSeo_TwitterCardFieldModel.php
@@ -6,23 +6,23 @@ class SproutSeo_TwitterCardFieldModel extends BaseModel
 	protected function defineAttributes()
 	{
 		return array(
-			'twitterCard'                         => AttributeType::String,
+			'twitterCard'                    => AttributeType::String,
 			
 			// Fields for all twitter cards
-			'twitterSite'                         => AttributeType::String,
-			'twitterTitle'                        => AttributeType::String,
-			'twitterCreator'                      => AttributeType::String,
-			'twitterDescription'                  => AttributeType::String,
+			'twitterSite'                    => AttributeType::String,
+			'twitterTitle'                   => AttributeType::String,
+			'twitterCreator'                 => AttributeType::String,
+			'twitterDescription'             => AttributeType::String,
 			
-			'twitterUrl'                          => array(AttributeType::String),
-			'twitterImage'                        => array(AttributeType::String),
+			'twitterUrl'                     => array(AttributeType::String),
+			'twitterImage'                   => array(AttributeType::String),
 			
 			// Fields for Player Card
-			'twitterPlayer'                       => AttributeType::String,
-			'twitterPlayerStream'                 => AttributeType::String,
-			'twitterPlayerStreamContentType'      => AttributeType::String,
-			'twitterPlayerWidth'                  => AttributeType::String,
-			'twitterPlayerHeight'                 => AttributeType::String,
+			'twitterPlayer'                  => AttributeType::String,
+			'twitterPlayerStream'            => AttributeType::String,
+			'twitterPlayerStreamContentType' => AttributeType::String,
+			'twitterPlayerWidth'             => AttributeType::String,
+			'twitterPlayerHeight'            => AttributeType::String,
 		);
 	}
 }
