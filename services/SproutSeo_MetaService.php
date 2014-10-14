05a8e817b3beb8839ab6f38d18fb769616426869
Noah Welch <noah@noahwelch.com>
2014-10-14 12:43:55 -0400

    Added image url check to twitterImage

--- services/SproutSeo_MetaService.php
+++ services/SproutSeo_MetaService.php
@@ -566,7 +566,12 @@ class SproutSeo_MetaService extends BaseApplicationComponent
 
 			if (!empty($twitterImage)) 
 			{
-				$metaValues['twitterImage'] = UrlHelper::getSiteUrl($twitterImage->url);
+				// check to see if Asset already has full Site Url in folder Url
+				if (strpos($twitterImage->url, UrlHelper::getSiteUrl()) !== false) {
+					$metaValues['twitterImage'] = $twitterImage->url;
+				} else {
+					$metaValues['twitterImage'] = UrlHelper::getSiteUrl($twitterImage->url);
+				}
 			}
 		}
 
