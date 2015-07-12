1c47c886b829fbc095c80f1a0cee5353b8c41aef
Ben Parizek <ben@barrelstrengthdesign.com>
2015-07-12 00:52:50 -0700

    Adds getMetaTagData and getMetaTagName to field models
    - Adds getMetaTagData method to all field models
    - Adds getMetaTagName method to map some fields to different template names

--- models/SproutSeo_GeographicMetaFieldModel.php
+++ models/SproutSeo_GeographicMetaFieldModel.php
@@ -9,9 +9,47 @@ class SproutSeo_GeographicMetaFieldModel extends BaseModel
 		return array(
 			'region'    => AttributeType::String,
 			'placename' => AttributeType::String,
+			'position'  => AttributeType::String,
 			'latitude'  => AttributeType::String,
 			'longitude' => AttributeType::String
 		);
 	}
 
+	public function getMetaTagData(SproutSeo_MetaModel $meta)
+	{
+		$tagData = array();
+
+		foreach ($this->getAttributes() as $key => $value)
+		{
+			if ($key == 'latitude' or $key == 'longitude')
+			{
+				break;
+			}
+
+			if ($meta->{$key})
+			{
+				$value = $meta[$key];
+
+				if ($key == 'position')
+				{
+					$value = SproutSeoMetaHelper::prepareGeoPosition($meta);
+				}
+
+				$tagData[$this->getMetaTagName($key)] = $value;
+			}
+		}
+
+		return $tagData;
+	}
+
+	public function getMetaTagName($handle)
+	{
+		$tagNames = array(
+			'region'    => 'geo.region',
+			'placename' => 'geo.placename',
+			'position'  => 'geo.position'
+		);
+
+		return $tagNames[$handle];
+	}
 }
--- models/SproutSeo_OpenGraphFieldModel.php
+++ models/SproutSeo_OpenGraphFieldModel.php
@@ -9,14 +9,59 @@ class SproutSeo_OpenGraphFieldModel extends BaseModel
 			'ogTitle'        => array(AttributeType::String),
 			'ogType'         => array(AttributeType::String),
 			'ogUrl'          => array(AttributeType::String),
+
 			'ogImage'        => array(AttributeType::Number),
+
 			'ogAuthor'       => array(AttributeType::String),
 			'ogPublisher'    => array(AttributeType::String),
+
 			'ogSiteName'     => array(AttributeType::String),
 			'ogDescription'  => array(AttributeType::String),
 			'ogAudio'        => array(AttributeType::String),
 			'ogVideo'        => array(AttributeType::String),
 			'ogLocale'       => array(AttributeType::String),
 		);
+	}
+
+	public function getMetaTagData(SproutSeo_MetaModel $meta)
+	{
+		$tagData = array();
+
+		foreach ($this->getAttributes() as $key => $value)
+		{
+			if ($meta->{$key})
+			{
+				$value = craft()->config->parseEnvironmentString($meta->{$key});
+				$tagData[$this->getMetaTagName($key)] = $value;
+			}
+		}
+
+		return $tagData;
+	}
+
+	public function getMetaTagName($handle)
+	{
+		$tagNames = array(
+			'ogTitle'        => 'og:title',
+			'ogType'         => 'og:type',
+			'ogUrl'          => 'og:url',
+
+			'ogImage'        => 'og:image',
+			'ogImageSecure'  => 'og:image:secure_url',
+			'ogImageWidth'   => 'og:image:width',
+			'ogImageHeight'  => 'og:image:height',
+			'ogImageType'    => 'og:image:type',
+
+			'ogAuthor'       => 'og:author',
+			'ogPublisher'    => 'og:publisher',
+
+			'ogSiteName'     => 'og:site_name',
+			'ogDescription'  => 'og:description',
+			'ogAudio'        => 'og:audio',
+			'ogVideo'        => 'og:video',
+			'ogLocale'       => 'og:locale',
+		);
+
+		return $tagNames[$handle];
 	}
 }
