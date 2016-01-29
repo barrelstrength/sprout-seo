401d33fdc4d4d15859bdb8aed448593a0778792f
Andre Lopez <andre@barrelstrengthdesign.com>
2016-01-29 14:51:06 -0500

    Updates migration to use best practices

--- migrations/m160127_000000_sproutSeo_addDefaultSettings.php
+++ migrations/m160127_000000_sproutSeo_addDefaultSettings.php
@@ -17,23 +17,22 @@ class m160127_000000_sproutSeo_addDefaultSettings extends BaseMigration
 		{
 			$structureId = sproutSeo()->redirects->installDefaultSettings($settings->pluginNameOverride);
 			SproutSeoPlugin::log('Successfully added structure', LogLevel::Info, true);
-			#$redirects = SproutSeo_RedirectRecord::model()->findAll();
 
 			// Find all currents redirects
 			$redirects = craft()->db->createCommand()
 				->select('*')
-				->from('sproutseo_redirects');
+				->from('sproutseo_redirects')
+				->queryAll();
 
 			// Set structure to currents redirects
 			foreach ($redirects as $key => $redirect)
 			{
 				$redirectModel = new SproutSeo_RedirectModel;
-				$redirectModel->id     = $redirect->id;
-				$redirectModel->oldUrl = $redirect->oldUrl;
-				$redirectModel->newUrl = $redirect->newUrl;
-				$redirectModel->method = $redirect->method;
-				$redirectModel->regex  = $redirect->regex;
-				SproutSeoPlugin::log('Structured! '.$redirectModel->id, LogLevel::Info, true);
+				$redirectModel->id     = $redirect["id"];
+				$redirectModel->oldUrl = $redirect["oldUrl"];
+				$redirectModel->newUrl = $redirect["newUrl"];
+				$redirectModel->method = $redirect["method"];
+				$redirectModel->regex  = $redirect["regex"];
 
 				craft()->structures->appendToRoot($structureId, $redirectModel);
 			}
