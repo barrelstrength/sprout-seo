f926cf4815e673c8856fc52cf4c23308c9edd244
Andre Lopez <andre@barrelstrengthdesign.com>
2016-03-02 11:09:21 -0500

    Fixes bug when the settings are updated. #760077

--- services/SproutSeo_SettingsService.php
+++ services/SproutSeo_SettingsService.php
@@ -9,7 +9,24 @@ class SproutSeo_SettingsService extends BaseApplicationComponent
 	 */
 	public function saveSettings($settings)
 	{
-		$settings = JsonHelper::encode($settings);
+		$plugin = craft()->plugins->getPlugin('sproutseo');
+		$seoSettings = $plugin->getSettings();
+
+		if (isset($settings["pluginNameOverride"]))
+		{
+			$seoSettings->pluginNameOverride = $settings["pluginNameOverride"] != null ?
+				$settings["pluginNameOverride"] :
+				$seoSettings->pluginNameOverride;
+		}
+
+		if (isset($settings["seoDivider"]))
+		{
+			$seoSettings->seoDivider = $settings["seoDivider"] != null ?
+				$settings["seoDivider"] :
+				$seoSettings->seoDivider;
+		}
+
+		$settings = JsonHelper::encode($seoSettings);
 
 		$affectedRows = craft()->db->createCommand()->update('plugins', array(
 			'settings' => $settings
