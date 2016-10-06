e040616ae07071e579418016c34aabc6a19e8217
Ben Parizek <ben@barrelstrengthdesign.com>
2016-10-06 16:18:28 -0700

    Sprout SEO v2.4.0
    Bumping up the version number as we are beginning to share in private beta.

--- SproutSeoPlugin.php
+++ SproutSeoPlugin.php
@@ -31,7 +31,7 @@ class SproutSeoPlugin extends BasePlugin
 	 */
 	public function getVersion()
 	{
-		return '2.3.0';
+		return '2.4.0';
 	}
 
 	/**
@@ -39,7 +39,7 @@ class SproutSeoPlugin extends BasePlugin
 	 */
 	public function getSchemaVersion()
 	{
-		return '2.3.0';
+		return '2.4.0';
 	}
 
 	/**
