a2e67c0bab408f19be0cafb3d4aba1b045f1fb67
Ben Parizek <ben@barrelstrengthdesign.com>
2016-10-12 16:56:48 -0700

    Refactors SectionMetadataService to memoize preparation of URL-Enabled Sections
    - Removes initialization from __construct method because the intialization triggers a database query for a table that does not exist until the migration is complete.

--- services/SproutSeo_SectionMetadataService.php
+++ services/SproutSeo_SectionMetadataService.php
@@ -25,8 +25,6 @@ class SproutSeo_SectionMetadataService extends BaseApplicationComponent
 	 */
 	public function __construct($sectionMetadataRecord = null)
 	{
-		$this->prepareUrlEnabledSectionTypes();
-
 		$this->sectionMetadataRecord = $sectionMetadataRecord;
 		if (is_null($this->sectionMetadataRecord))
 		{
@@ -39,6 +37,12 @@ class SproutSeo_SectionMetadataService extends BaseApplicationComponent
 	 */
 	public function prepareUrlEnabledSectionTypes()
 	{
+		// Have we already prepared our URL-Enabled Sections?
+		if (count($this->urlEnabledSectionTypes))
+		{
+			return null;
+		}
+
 		$registeredUrlEnabledSectionsTypes = craft()->plugins->call('registerSproutSeoUrlEnabledSectionTypes');
 
 		foreach ($registeredUrlEnabledSectionsTypes as $plugin => $urlEnabledSectionTypes)
@@ -115,6 +119,8 @@ class SproutSeo_SectionMetadataService extends BaseApplicationComponent
 	 */
 	public function getUrlEnabledSectionsViaContext($context)
 	{
+		$this->prepareUrlEnabledSectionTypes();
+
 		foreach ($this->urlEnabledSectionTypes as $urlEnabledSectionType)
 		{
 			$urlEnabledSectionType->typeIdContext = 'matchedElementCheck';
@@ -148,6 +154,8 @@ class SproutSeo_SectionMetadataService extends BaseApplicationComponent
 	 */
 	public function getUrlEnabledSectionTypeByType($type)
 	{
+		$this->prepareUrlEnabledSectionTypes();
+
 		foreach ($this->urlEnabledSectionTypes as $urlEnabledSectionType)
 		{
 			if ($urlEnabledSectionType->getId() == $type)
@@ -168,6 +176,8 @@ class SproutSeo_SectionMetadataService extends BaseApplicationComponent
 	 */
 	public function getUrlEnabledSectionTypeByElementType($elementType)
 	{
+		$this->prepareUrlEnabledSectionTypes();
+
 		foreach ($this->urlEnabledSectionTypes as $urlEnabledSectionType)
 		{
 			if ($urlEnabledSectionType->getElementType() == $elementType)
