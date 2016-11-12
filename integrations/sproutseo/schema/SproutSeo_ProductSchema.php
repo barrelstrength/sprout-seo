02d080c456d6348e395c5e8df1bbf8eee8e8755b
Ben Parizek <ben@barrelstrengthdesign.com>
2016-11-12 12:46:14 -0800

    Adds support for Commerce_Products in Product Schema

--- integrations/sproutseo/schema/SproutSeo_ProductSchema.php
+++ integrations/sproutseo/schema/SproutSeo_ProductSchema.php
@@ -33,37 +33,59 @@ class SproutSeo_ProductSchema extends SproutSeo_ThingSchema
 	public function addProperties()
 	{
 		parent::addProperties();
+
+		$elementType = $this->element->getElementType();
+
+		if ($elementType == 'Commerce_Product')
+		{
+			$this->addCommerceProductProperties();
+		}
+		else
+		{
+			parent::addProperties();
+		}
 	}
 
-	// <script type="application/ld+json">
-	// {
-	//   "@context": ,
-	//   "@type": "Product",
-	//   "name": "Executive Anvil",
-	//   "image": "http://www.example.com/anvil_executive.jpg",
-	//   "description": "Sleeker than ACME's Classic Anvil, the Executive Anvil is perfect for the business traveler looking for something to drop from a height.",
-	//   "mpn": "925872",
-	//   "brand": {
-	//     "@type": "Thing",
-	//     "name": "ACME"
-	//   },
-	//   "aggregateRating": {
-	//     "@type": "AggregateRating",
-	//     "ratingValue": "4.4",
-	//     "reviewCount": "89"
-	//   },
-	//   "offers": {
-	//     "@type": "Offer",
-	//     "priceCurrency": "USD",
-	//     "price": "119.99",
-	//     "priceValidUntil": "2020-11-05",
-	//     "itemCondition": "http://schema.org/UsedCondition",
-	//     "availability": "http://schema.org/InStock",
-	//     "seller": {
-	//       "@type": "Organization",
-	//       "name": "Executive Objects"
-	//     }
-	//   }
-	// }
-	// </script>
+	public function addCommerceProductProperties()
+	{
+		$identity = $this->globals['identity'];
+		$element  = $this->element;
+
+		$primaryCurrencyIso = craft()->commerce_paymentCurrencies->getPrimaryPaymentCurrencyIso();
+
+		$offers = array();
+
+		if ($identityType = $identity['@type'])
+		{
+			// Determine if we have an Organization or Person Schema Type
+			$schemaModel = 'Craft\SproutSeo_WebsiteIdentity' . $identityType . 'Schema';
+
+			$identitySchema          = new $schemaModel();
+			$identitySchema->globals = $this->globals;
+			$seller                  = $identitySchema->getSchema();
+		}
+
+		foreach ($element->variants as $variant)
+		{
+
+			$offers[$variant->id]['@type']         = 'Offer';
+			$offers[$variant->id]['sku']           = $variant->sku;
+			$offers[$variant->id]['price']         = $variant->price;
+			$offers[$variant->id]['priceCurrency'] = $primaryCurrencyIso;
+
+			if ($variant->unlimitedStock == 1 OR $variant->stock > 0)
+			{
+				$availability = 'https://schema.org/InStock';
+			}
+			else
+			{
+				$availability = 'https://schema.org/OutOfStock';
+			}
+
+			$offers[$variant->id]['availability'] = $availability;
+			$offers[$variant->id]['seller']       = $seller;
+		}
+
+		$this->addProperty('offers', array_values($offers));
+	}
 }
