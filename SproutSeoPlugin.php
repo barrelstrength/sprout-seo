cb9d7b2a881176f92610876936fe719465cb6e6e
Andre Lopez <andre@barrelstrengthdesign.com>
2016-11-30 18:14:05 -0400

    Sprout SEO v3.0.5 - Private Beta

--- SproutSeoPlugin.php
+++ SproutSeoPlugin.php
@@ -37,7 +37,7 @@ class SproutSeoPlugin extends BasePlugin
 	 */
 	public function getVersion()
 	{
-		return '3.0.4';
+		return '3.0.5';
 	}
 
 	/**
@@ -45,7 +45,7 @@ class SproutSeoPlugin extends BasePlugin
 	 */
 	public function getSchemaVersion()
 	{
-		return '3.0.3';
+		return '3.0.5';
 	}
 
 	/**
