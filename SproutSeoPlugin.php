84e2236077ca24c90f6f2d11ab4a4f00a0b5322b
Ben Parizek <ben@barrelstrengthdesign.com>
2016-12-05 07:46:46 -0800

    Sprout SEO v3.0.6

--- SproutSeoPlugin.php
+++ SproutSeoPlugin.php
@@ -37,7 +37,7 @@ class SproutSeoPlugin extends BasePlugin
 	 */
 	public function getVersion()
 	{
-		return '3.0.5';
+		return '3.0.6';
 	}
 
 	/**
@@ -45,7 +45,7 @@ class SproutSeoPlugin extends BasePlugin
 	 */
 	public function getSchemaVersion()
 	{
-		return '3.0.5';
+		return '3.0.6';
 	}
 
 	/**
