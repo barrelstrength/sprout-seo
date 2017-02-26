bb1cf8e7685ad71e8d2ec01f9a38d752f2b198f8
Ben Parizek <ben@barrelstrengthdesign.com>
2017-02-26 09:38:38 -0800

    Code cleanup

--- records/SproutSeo_ElementMetadataRecord.php
+++ records/SproutSeo_ElementMetadataRecord.php
@@ -17,18 +17,18 @@ class SproutSeo_ElementMetadataRecord extends BaseRecord
 	public function defineAttributes()
 	{
 		return array(
-			'elementId'             => array(AttributeType::Number, 'required' => true),
-			'locale'                => array(AttributeType::Locale, 'required' => true),
+			'elementId'            => array(AttributeType::Number, 'required' => true),
+			'locale'               => array(AttributeType::Locale, 'required' => true),
 
 			// Optimized Meta
-			'optimizedTitle'        => array(AttributeType::String, 'required' => false),
-			'optimizedDescription'  => array(AttributeType::String, 'required' => false),
-			'optimizedImage'        => array(AttributeType::String, 'required' => false),
-			'optimizedKeywords'     => array(AttributeType::String, 'required' => false),
+			'optimizedTitle'       => array(AttributeType::String, 'required' => false),
+			'optimizedDescription' => array(AttributeType::String, 'required' => false),
+			'optimizedImage'       => array(AttributeType::String, 'required' => false),
+			'optimizedKeywords'    => array(AttributeType::String, 'required' => false),
 
 			// Structured Data
-			'schemaTypeId'                   => array(AttributeType::String),
-			'schemaOverrideTypeId'           => array(AttributeType::String),
+			'schemaTypeId'         => array(AttributeType::String),
+			'schemaOverrideTypeId' => array(AttributeType::String),
 
 			'enableMetaDetailsSearch'      => array(AttributeType::Bool, 'default' => false, 'required' => false),
 			'enableMetaDetailsOpenGraph'   => array(AttributeType::Bool, 'default' => false, 'required' => false),
@@ -47,27 +47,27 @@ class SproutSeo_ElementMetadataRecord extends BaseRecord
 			'latitude'  => array(AttributeType::String),
 			'longitude' => array(AttributeType::String),
 
-			'ogType'      => array(AttributeType::String),
-			'ogUrl'       => array(AttributeType::String),
-			'ogSiteName'  => array(AttributeType::String),
-			'ogTitle'     => array(AttributeType::String),
+			'ogType'        => array(AttributeType::String),
+			'ogUrl'         => array(AttributeType::String),
+			'ogSiteName'    => array(AttributeType::String),
+			'ogTitle'       => array(AttributeType::String),
 			'ogDescription' => array(AttributeType::String),
-			'ogImage'     => array(AttributeType::String),
-			'ogTransform' => array(AttributeType::String),
-			'ogAuthor'    => array(AttributeType::String),
-			'ogPublisher' => array(AttributeType::String),
+			'ogImage'       => array(AttributeType::String),
+			'ogTransform'   => array(AttributeType::String),
+			'ogAuthor'      => array(AttributeType::String),
+			'ogPublisher'   => array(AttributeType::String),
 			'ogAudio'       => array(AttributeType::String),
 			'ogVideo'       => array(AttributeType::String),
 			'ogLocale'      => array(AttributeType::String),
 
-			'twitterCard'        => array(AttributeType::String),
+			'twitterCard'                    => array(AttributeType::String),
 			'twitterUrl'                     => array(AttributeType::String),
-			'twitterSite'        => array(AttributeType::String),
-			'twitterTitle'       => array(AttributeType::String),
-			'twitterDescription' => array(AttributeType::String),
+			'twitterSite'                    => array(AttributeType::String),
+			'twitterTitle'                   => array(AttributeType::String),
+			'twitterDescription'             => array(AttributeType::String),
 			'twitterImage'                   => array(AttributeType::String),
 			'twitterTransform'               => array(AttributeType::String),
-			'twitterCreator'     => array(AttributeType::String),
+			'twitterCreator'                 => array(AttributeType::String),
 
 			// Fields for Twitter Player Card
 			'twitterPlayer'                  => array(AttributeType::String),
