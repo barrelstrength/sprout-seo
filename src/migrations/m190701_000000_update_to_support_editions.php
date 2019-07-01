3561220a593c146dc8b28f15d334b07d139fa0c5
Ben Parizek <ben@barrelstrengthdesign.com>
2019-07-01 15:41:44 -0400

    Adds Pro Edition migration

---
+++ src/migrations/m190701_000000_update_to_support_editions.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace barrelstrength\sproutseo\migrations;
+
+use barrelstrength\sproutseo\SproutSeo;
+use craft\db\Migration;
+use Craft;
+use craft\errors\InvalidPluginException;
+use Throwable;
+
+/**
+ * m190701_000000_update_to_support_editions migration.
+ */
+class m190701_000000_update_to_support_editions extends Migration
+{
+    /**
+     * @inheritdoc
+     *
+     * @return bool
+     * @throws Throwable
+     * @throws InvalidPluginException
+     */
+    public function safeUp(): bool
+    {
+        // Don't make the same config changes twice
+        $projectConfig = Craft::$app->getProjectConfig();
+        $schemaVersion = $projectConfig->get('plugins.sprout-seo.schemaVersion', true);
+
+        if (version_compare($schemaVersion, '4.1.0', '>=')) {
+            return true;
+        }
+
+        Craft::$app->getPlugins()->switchEdition('sprout-seo', SproutSeo::EDITION_PRO);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m190701_000000_update_to_support_editions cannot be reverted.\n";
+        return false;
+    }
+}
