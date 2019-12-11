4f995a67adbeb206ec85b8c4289b5361f1d1dda0
Ben Parizek <ben.parizek@gmail.com>
2019-12-10 23:11:10 -0500

    Fixes dateLastUsed column type

---
+++ src/migrations/m191210_000000_update_dateLastUsed_column_type_sproutseo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace barrelstrength\sproutseo\migrations;
+
+use barrelstrength\sproutbaseredirects\migrations\m191109_000001_update_field_regex_to_matchStrategy;
+use barrelstrength\sproutbaseredirects\migrations\m191210_000000_update_dateLastUsed_column_type;
+use craft\db\Migration;
+use Throwable;
+
+/**
+ * m191210_000000_update_dateLastUsed_column_type_sproutseo migration.
+ */
+class m191210_000000_update_dateLastUsed_column_type_sproutseo extends Migration
+{
+    /**
+     * @inheritdoc
+     *
+     * @return bool
+     * @throws Throwable
+     */
+    public function safeUp(): bool
+    {
+        $migration = new m191210_000000_update_dateLastUsed_column_type();
+
+        ob_start();
+        $migration->safeUp();
+        ob_end_clean();
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m191210_000000_update_dateLastUsed_column_type_sproutseo cannot be reverted.\n";
+        return false;
+    }
+}
