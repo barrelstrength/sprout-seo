1893dc3f99133fa8579cb5d763e7e9082f603755
Ben Parizek <ben.parizek@gmail.com>
2020-01-20 09:22:02 -0500

    Move website identity js logic into class SproutSeoWebsiteIdentity

--- src/web/assets/seo/src/js/website-identity.js
+++ src/web/assets/seo/src/js/website-identity.js
@@ -1,133 +1,207 @@
-/*
- * @link      https://sprout.barrelstrengthdesign.com/
- * @copyright Copyright (c) Barrel Strength Design LLC
- * @license   http://sprout.barrelstrengthdesign.com/license
- */
+class SproutSeoWebsiteIdentity {
+    constructor(props) {
+        this.items = props.items;
+        this.websiteIdentity = props.websiteIdentity;
 
-'use strict';
+        this.initWebsiteIdentityField();
+        this.moreWebsiteIdentityStuff();
+        this.initKeywordsField();
+    }
 
-$(document).ready(function() {
+    initWebsiteIdentityField() {
+        let self = this;
 
-    // Default option
-    var option = '<option value="" selected="selected"></option>';
+        // Default option
+        let option = '<option value="" selected="selected"></option>';
 
-    // Method to clear dropdowns down to a given level
-    var clearDropDown = function(arrayObj, startIndex) {
-        $.each(arrayObj, function(index, value) {
-            if (index >= startIndex) {
-                $(value).html(option);
-            }
-        });
-    };
-
-    // Method to disable dropdowns down to a given level
-    var disableDropDown = function(arrayObj, startIndex) {
-        $.each(arrayObj, function(index, value) {
-            if (index >= startIndex) {
-                $(value).closest('div.organizationinfo-dropdown').addClass('hidden');
-            }
-        });
-    };
-
-    // Method to enable dropdowns down to a given level
-    var enableDropDown = function(element) {
-        element.closest('div.organizationinfo-dropdown').removeClass('hidden');
-    };
-
-    // Method to generate and append options
-    var generateOptions = function(element, children) {
-        var options, name = '';
-
-        for (var i = 0; i < children.length; i++) {
-            // insert space before capital letters
-            name = children[i].name.replace(/([A-Z][^A-Z\b])/g, ' $1').trim();
-
-            options += '<option value="' + children[i].name + '">' + name + '</option>';
-        }
-        element.append(options);
-    };
-
-    // Select each of the dropdowns
-    var firstDropDown = $('#first');
-    var secondDropDown = $('#second');
-    var thirdDropDown = $('#third');
-
-    // Hold selected option
-    var firstSelection = '';
-    var secondSelection = '';
-    var thirdSelection = '';
-
-    // Hold selection
-    var selection = '';
-
-    // Selection handler for first level dropdown
-    firstDropDown.on('change', function() {
-
-        // Get selected option
-        firstSelection = firstDropDown.val();
-
-        // Clear all dropdowns down to the hierarchy
-        clearDropDown($("#organization :input"), 1);
-
-        // Disable all dropdowns down to the hierarchy
-        disableDropDown($("#organization :input"), 1);
-
-        // Check current selection
-        if (firstSelection === '') {
-            return;
-        }
-
-        if (items[firstSelection].hasOwnProperty('children')) {
-            // Enable second level DropDown
-            enableDropDown(secondDropDown);
-
-            // Generate and append options
-            generateOptions(secondDropDown, items[firstSelection]['children']);
-        }
-
-    });
-
-    // Selection handler for second level dropdown
-    secondDropDown.on('change', function() {
-
-        firstSelection = $('#first').val();
-        secondSelection = secondDropDown.val();
-
-        // Clear all dropdowns down to the hierarchy
-        clearDropDown($("#organization :input"), 2);
-
-        // Disable all dropdowns down to the hierarchy
-        disableDropDown($("#organization :input"), 2);
-
-        // Check current selection
-        if (secondSelection === '') {
-            return;
-        }
-
-        var secondChildren = [];
-        var children = items[firstSelection]['children'];
-        var pos = null;
-        for (var i = 0; i < children.length; i++) {
-            if (children[i].name === secondSelection) {
-                if (children[i].hasOwnProperty('children')) {
-                    // Enable third level DropDown
-                    enableDropDown(thirdDropDown);
-
-                    // Generate and append options
-                    generateOptions(thirdDropDown, children[i]['children']);
+        // Method to clear dropdowns down to a given level
+        let clearDropDown = function(arrayObj, startIndex) {
+            $.each(arrayObj, function(index, value) {
+                if (index >= startIndex) {
+                    $(value).html(option);
                 }
+            });
+        };
 
-                break;
+        // Method to disable dropdowns down to a given level
+        let disableDropDown = function(arrayObj, startIndex) {
+            $.each(arrayObj, function(index, value) {
+                if (index >= startIndex) {
+                    $(value).closest('div.organizationinfo-dropdown').addClass('hidden');
+                }
+            });
+        };
+
+        // Method to enable dropdowns down to a given level
+        let enableDropDown = function(element) {
+            element.closest('div.organizationinfo-dropdown').removeClass('hidden');
+        };
+
+        // Method to generate and append options
+        let generateOptions = function(element, children) {
+            let options, name = '';
+
+            for (let i = 0; i < children.length; i++) {
+                // insert space before capital letters
+                name = children[i].name.replace(/([A-Z][^A-Z\b])/g, ' $1').trim();
+
+                options += '<option value="' + children[i].name + '">' + name + '</option>';
+            }
+            element.append(options);
+        };
+
+        // Select each of the dropdowns
+        let firstDropDown = $('#first');
+        let secondDropDown = $('#second');
+        let thirdDropDown = $('#third');
+
+        // Hold selected option
+        let firstSelection = '';
+        let secondSelection = '';
+        let thirdSelection = '';
+
+        // Hold selection
+        let selection = '';
+
+        // Selection handler for first level dropdown
+        firstDropDown.on('change', function() {
+
+            // Get selected option
+            firstSelection = firstDropDown.val();
+
+            // Clear all dropdowns down to the hierarchy
+            clearDropDown($("#organization :input"), 1);
+
+            // Disable all dropdowns down to the hierarchy
+            disableDropDown($("#organization :input"), 1);
+
+            // Check current selection
+            if (firstSelection === '') {
+                return;
+            }
+
+            if (self.items[firstSelection].hasOwnProperty('children')) {
+                // Enable second level DropDown
+                enableDropDown(secondDropDown);
+
+                // Generate and append options
+                generateOptions(secondDropDown, self.items[firstSelection]['children']);
+            }
+
+        });
+
+        // Selection handler for second level dropdown
+        secondDropDown.on('change', function() {
+
+            firstSelection = $('#first').val();
+            secondSelection = secondDropDown.val();
+
+            // Clear all dropdowns down to the hierarchy
+            clearDropDown($("#organization :input"), 2);
+
+            // Disable all dropdowns down to the hierarchy
+            disableDropDown($("#organization :input"), 2);
+
+            // Check current selection
+            if (secondSelection === '') {
+                return;
+            }
+
+            let secondChildren = [];
+            let children = self.items[firstSelection]['children'];
+            let pos = null;
+            for (let i = 0; i < children.length; i++) {
+                if (children[i].name === secondSelection) {
+                    if (children[i].hasOwnProperty('children')) {
+                        // Enable third level DropDown
+                        enableDropDown(thirdDropDown);
+
+                        // Generate and append options
+                        generateOptions(thirdDropDown, children[i]['children']);
+                    }
+
+                    break;
+                }
+            }
+
+        });
+
+        // Selection handler for third level dropdown
+        thirdDropDown.on('change', function() {
+            thirdSelection = thirdDropDown.val();
+            // Final work goes here
+
+        });
+    }
+
+    moreWebsiteIdentityStuff() {
+        let websiteIdentity = this.websiteIdentity;
+
+        // check if we need load depending dropdowns
+        if (websiteIdentity)
+        {
+            if (websiteIdentity.hasOwnProperty('organizationSubTypes') && websiteIdentity.organizationSubTypes[0])
+            {
+                $('#first').val(websiteIdentity.organizationSubTypes[0]).change();
+            }
+            if (websiteIdentity.hasOwnProperty('organizationSubTypes') && websiteIdentity.organizationSubTypes[1])
+            {
+                $('#second').val(websiteIdentity.organizationSubTypes[1]).change();
+            }
+            if (websiteIdentity.hasOwnProperty('organizationSubTypes') && websiteIdentity.organizationSubTypes[2])
+            {
+                $('#third').val(websiteIdentity.organizationSubTypes[2]).change();
             }
         }
 
-    });
+        $( "#identityType" ).change(function() {
 
-    // Selection handler for third level dropdown
-    thirdDropDown.on('change', function() {
-        thirdSelection = thirdDropDown.val();
-        // Final work goes here
+            if(this.value === 'Person')
+            {
+                $( ".person-info" ).removeClass('hidden');
+                $( ".organization-info" ).addClass('hidden');
+            }
+            else
+            {
+                $( ".person-info" ).addClass('hidden');
+                $( ".organization-info" ).removeClass('hidden');
+            }
 
-    });
+            if(this.value === 'Organization')
+            {
+                $( ".organization-info" ).removeClass('hidden');
+                $( ".person-info" ).addClass('hidden');
 
-});
+                if ($( "#first" ).val() == 'LocalBusiness')
+                {
+                    $( "#localbusiness" ).removeClass('hidden');
+                }
+            }
+            else
+            {
+                $( ".organization-info" ).addClass('hidden');
+                $( ".person-info" ).removeClass('hidden');
+            }
+        });
+
+        $( "#first" ).change(function() {
+            if(this.value === 'LocalBusiness')
+            {
+                $( "#localbusiness" ).removeClass('hidden');
+            }
+            else
+            {
+                $( "#localbusiness" ).addClass('hidden');
+            }
+        });
+    }
+
+    initKeywordsField() {
+        $('#keywords-field input').tagEditor({
+            animateDelete: 20
+        });
+    }
+}
+
+window.SproutSeoWebsiteIdentity = SproutSeoWebsiteIdentity;
+
