e782d0a785705b742dd500bf8f15fcfcf4f2b8a5
Ben Parizek <ben.parizek@gmail.com>
2020-01-29 11:45:20 -0500

    Update copyright

--- src/web/assets/seo/src/js/meta-details-toggle.js
+++ src/web/assets/seo/src/js/meta-details-toggle.js
@@ -1,3 +1,9 @@
+/*
+ * @link https://sprout.barrelstrengthdesign.com
+ * @copyright Copyright (c) Barrel Strength Design LLC
+ * @license https://craftcms.github.io/license
+ */
+
 /**
  * Helper class to toggle groups of sub-fields on Open Graph and Twitter Card Meta Details tabs
  * Initialize the class and identify the target select field to watch. When the select field is
