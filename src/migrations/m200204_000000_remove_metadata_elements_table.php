199e819c155e18abd5ad2a058502239b08defd01
Ben Parizek <ben.parizek@gmail.com>
2020-02-05 15:18:32 -0500

    Adds cleanup migration

---
+++ src/migrations/m200204_000000_remove_metadata_elements_table.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @link https://sprout.barrelstrengthdesign.com
+ * @copyright Copyright (c) Barrel Strength Design LLC
+ * @license https://craftcms.github.io/license
+ */
+
+namespace barrelstrength\sproutfields\migrations;
+
+use craft\db\Migration;
+
+/**
+ * m200204_000000_remove_metadata_elements_table migration.
+ */
+class m200204_000000_remove_metadata_elements_table extends Migration
+{
+    /**
+     * @return bool
+     */
+    public function safeUp(): bool
+    {
+        $this->dropTableIfExists('{{%sproutseo_metadata_elements}}');
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m200204_000000_remove_metadata_elements_table cannot be reverted.\n";
+
+        return false;
+    }
+}
