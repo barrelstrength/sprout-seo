72f040476101291a1916013edb660bcb3c7e386e
Ben Parizek <ben.parizek@gmail.com>
2020-03-01 20:09:47 -0500

    Initial SEO Preview controller file

--- src/controllers/SeoPreviewController.php
+++ src/controllers/SeoPreviewController.php
@@ -7,9 +7,16 @@
 
 namespace barrelstrength\sproutseo\controllers;
 
+use barrelstrength\sproutseo\SproutSeo;
 use Craft;
 use craft\base\Element;
 use craft\web\Controller;
+use craft\web\View;
+use Throwable;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use yii\base\Exception;
 use yii\web\ForbiddenHttpException;
 use yii\web\Response;
 
@@ -20,15 +27,37 @@ class SeoPreviewController extends Controller
      * @param int $siteId
      *
      * @return Response
+     * @throws Exception
      * @throws ForbiddenHttpException
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws Throwable
      */
     public function actionPreview(int $elementId, int $siteId): Response
     {
         // Protect the preview target from public access:
         $this->requireAuthorization('sproutSeoPreviewAuthorizationKey:' . $elementId);
 
+        // Start fresh
+        SproutSeo::$app->optimize->element = null;
+
         /** @var Element $element */
         $element = Craft::$app->getElements()->getElementById($elementId, null, $siteId);
-        return $this->asRaw($element->title);
+
+        if ($element) {
+            SproutSeo::$app->optimize->element = $element;
+        }
+
+        $site = Craft::$app->getSites()->getSiteById($siteId);
+
+        $metadata = SproutSeo::$app->optimize->getMetadata($site, false);
+
+        $html = Craft::$app->getView()->renderTemplate('sprout-seo/_preview/preview', [
+            'element' => $element,
+            'metadata' => $metadata
+        ], View::TEMPLATE_MODE_CP);
+
+        return $this->asRaw($html);
     }
 }
