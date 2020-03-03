508f567660a30acb3b8ffd523cac4826d2600a4f
Ben Parizek <ben.parizek@gmail.com>
2020-03-03 08:30:13 -0500

    Updates Element Metadata to handle null value scenarios

--- src/fields/ElementMetadata.php
+++ src/fields/ElementMetadata.php
@@ -115,6 +115,10 @@ class ElementMetadata extends Field
         $metadata = null;
         $metadataArray = null;
 
+        if ($value === null) {
+            return $value;
+        }
+
         // On page load and the resave element task the $value comes from the content table as json
         if (is_string($value)) {
             $metadataArray = Json::decode($value);
@@ -168,23 +172,25 @@ class ElementMetadata extends Field
         /** @var Element $element */
         $value = $element->getFieldValue($this->handle);
 
-        $metadataJson = null;
+        if ($value !== null) {
+            $metadataJson = null;
 
-        if ($value instanceof Metadata) {
-            $metadataJson = Json::encode($value->getRawData());
+            if ($value instanceof Metadata) {
+                $metadataJson = Json::encode($value->getRawData());
+            }
+
+            $contentTable = Craft::$app->getContent()->contentTable;
+            $fieldColumnPrefix = Craft::$app->getContent()->fieldColumnPrefix;
+            $fieldName = $fieldColumnPrefix.$this->handle;
+
+            Craft::$app->db->createCommand()->update($contentTable, [
+                $fieldName => $metadataJson
+            ], [
+                'and',
+                ['elementId' => $element->id],
+                ['siteId' => $element->siteId],
+            ], [], false)->execute();
         }
-
-        $contentTable = Craft::$app->getContent()->contentTable;
-        $fieldColumnPrefix = Craft::$app->getContent()->fieldColumnPrefix;
-        $fieldName = $fieldColumnPrefix.$this->handle;
-
-        Craft::$app->db->createCommand()->update($contentTable, [
-            $fieldName => $metadataJson
-        ], [
-            'and',
-            ['elementId' => $element->id],
-            ['siteId' => $element->siteId],
-        ], [], false)->execute();
 
         parent::afterElementSave($element, $isNew);
     }
