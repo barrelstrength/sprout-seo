f88decdd5b1cac9094dfb62d1e642fa04366ef91
Ben Parizek <ben.parizek@gmail.com>
2020-03-07 11:27:29 -0500

    Adds optimized value cleanup migration

--- src/SproutSeo.php
+++ src/SproutSeo.php
@@ -77,7 +77,7 @@ class SproutSeo extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '4.3.0.10';
+    public $schemaVersion = '4.3.0.15';
 
     /**
      * @var string
